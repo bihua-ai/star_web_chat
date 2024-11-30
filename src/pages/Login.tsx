342a3a6aee2adeb4b3137e343c0c397bdc7008ac
Eric Lee <eric99519@gmail.com>
2024-11-30 22:10:16 +0800

    save 2024.11.30

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('http://127.0.0.1:8000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
